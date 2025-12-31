2ed6b5aab6930cbafc3018728b85b4509c71cff4
Alok Kumar Maurya <100317862+alokmaurya22@users.noreply.github.com>
2026-01-01 00:04:25 +0530

    Duplicate experience entries for consistency

--- data/experienceData.js
+++ data/experienceData.js
@@ -9,6 +9,17 @@ const experienceData = [{
             "Developed and maintain the Library and Exam module in KNIPSS ERP, which automated exam scheduling and grading, and eliminated all manual paperwork in the library management for over 14000+ students.",
             "Made the ERP system more accurate and much faster, cutting down the time needed by 90%. Also took care of fixing problems, adding new features, and making sure the system ran smoothly"
         ]
+    },{
+        position: "Software Developer - Full Stack",
+        company: "WeKnow Technologies Pvt Ltd",
+        companyLink: "https://weknowtech.in/",
+        companyLogo: "images/exp-weknow.png",
+        duration: "Jan 2023 - Feb 2024",
+        responsibilities: [
+            "Engineered a streamlined digital solution for KNI Online admissions, cutting down processing time by 66% and creating a better experience for applicants.",
+            "Developed and maintain the Library and Exam module in KNIPSS ERP, which automated exam scheduling and grading, and eliminated all manual paperwork in the library management for over 14000+ students.",
+            "Made the ERP system more accurate and much faster, cutting down the time needed by 90%. Also took care of fixing problems, adding new features, and making sure the system ran smoothly"
+        ]
     },
     {
         position: "Web Developer Trainee",
@@ -23,5 +34,6 @@ const experienceData = [{
     }
 
 ];
+
 
 
